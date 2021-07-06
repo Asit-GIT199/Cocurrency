9f5a9bbdebcca07b24f41a077c9b96c2df956e40
Asit Kumar Mohanty <asit199@gmail.com>
2021-07-06 08:04:28 +0530

    Task Complettion Source.

---
+++ Winforms/Form4.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Winforms
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.LoadingGif = new System.Windows.Forms.PictureBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LoadingGif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(325, 109);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // LoadingGif
+            // 
+            this.LoadingGif.Image = ((System.Drawing.Image)(resources.GetObject("LoadingGif.Image")));
+            this.LoadingGif.Location = new System.Drawing.Point(286, 147);
+            this.LoadingGif.Name = "LoadingGif";
+            this.LoadingGif.Size = new System.Drawing.Size(228, 157);
+            this.LoadingGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.LoadingGif.TabIndex = 3;
+            this.LoadingGif.TabStop = false;
+            this.LoadingGif.Visible = false;
+            this.LoadingGif.WaitOnLoad = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(448, 111);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 4;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.LoadingGif);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)(this.LoadingGif)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox LoadingGif;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
---
+++ Winforms/Form4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winforms
+{
+    public partial class Form4 : Form
+    {
+        private string apiURL;
+        private HttpClient httpClient;
+        private CancellationTokenSource cts;
+
+        public Form4()
+        {
+            InitializeComponent();
+            apiURL = "https://localhost:44342/api";
+            httpClient = new HttpClient();
+        }
+
+
+        public Task EvaluateValue(string value)
+        {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+            if (value=="1")
+            {
+                tcs.SetResult(null);
+            }
+            else if(value=="2")
+            {
+                tcs.SetCanceled();
+
+            }
+            else
+            {
+                tcs.SetException(new ApplicationException($"Invalid Value {value}"));
+            }
+
+            return tcs.Task;
+        }
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            var task = EvaluateValue(txtInput.Text);
+
+            Console.WriteLine("begin");
+            Console.WriteLine($"Is Completed : {task.IsCompleted }");
+            Console.WriteLine($"Is Cancelled: {task.IsCanceled}");
+            Console.WriteLine($"Is faulted {task.IsFaulted}");
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Exception : {ex.Message}");
+            }
+        }
+    }
+}
